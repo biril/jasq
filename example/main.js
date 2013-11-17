7aecb0062a34cd5061ac0dd13fa26ce4c2636257
biril <alexlambiris@gmail.com>
2013-11-17 23:58:10 +0100

    De-lint example/main.js

--- example/main.js
+++ example/main.js
@@ -1,21 +1,28 @@
-// Configure require
-require.config({
-    baseUrl: "../test/testModules",
-    paths: {
-        squire: "../vendor/squire",
-        jasq: "../../jasq"
-    }
-});
+/* globals require:false, jasmine: false */
 
-// Configure Jasmine
-var jasmineEnv = jasmine.getEnv(),
-    htmlReporter = new jasmine.HtmlReporter();
-jasmineEnv.addReporter(htmlReporter);
-jasmineEnv.specFilter = function (spec) {
-    return htmlReporter.specFilter(spec);
-};
+(function () {
 
-// Require the spec and run suite once loaded
-require(["../../example/spec"], function () {
-    jasmineEnv.execute();
-});
+	"use strict";
+
+	// Configure require
+	require.config({
+	    baseUrl: "../test/testModules",
+	    paths: {
+	        jasq: "../../jasq"
+	    }
+	});
+
+	// Configure Jasmine
+	var jasmineEnv = jasmine.getEnv(),
+	    htmlReporter = new jasmine.HtmlReporter();
+	jasmineEnv.addReporter(htmlReporter);
+	jasmineEnv.specFilter = function (spec) {
+	    return htmlReporter.specFilter(spec);
+	};
+
+	// Require the spec and run suite once loaded
+	require(["../../example/spec"], function () {
+	    jasmineEnv.execute();
+	});
+
+}());
