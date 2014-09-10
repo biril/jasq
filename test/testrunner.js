2095bd63ab0b46567499429017eafcba3446884f
biril <alexlambiris@gmail.com>
2014-09-10 18:57:22 +0200

    Revise testrunner for QUnit/requirejs integration

--- test/testrunner.js
+++ test/testrunner.js
@@ -1,11 +1,11 @@
 /*jshint browser:true */
-/*global jasmineRequire:false, require:false */
+/*global QUnit:false, jasmineRequire:false, require:false */
 
 (function () {
 
     "use strict";
 
-    ////// Jasmine v >= 2.0.0 requires a bit of initialization:
+    ////// {{ Jasmine v >= 2.0.0 requires a bit of initialization:
 
     // Require Jasmine's core files. Specifically, this requires and attaches all of Jasmine's code
     //  to the `jasmine` reference
@@ -24,14 +24,25 @@
         }
     }
 
-    ////// Configure require and kick off test (for the latter, `require`-ing `test` is enough):
+    ////// }}
 
+    // Configure require and kick off tests
     require.config({
         baseUrl: "./testModules",
         paths: {
             jasq: "../../jasq",
             helpers: "../helpers"
         }
-    })(["../test", "../test-example"]);
+    })(["../test", "../test-example"], function () {
+
+        // `QUnit.start` _after_ loading the test suite(s). `QUnit.load` is also necessary as some
+        //  page elements (e.g. the modules-dropdown) will not render correctly unless the test
+        //  suites (that actually define the modules) load first. Note that all this assumes a
+        //  preceding `QUnit.config.autostart = false` - which needs to be set _after_ the qunit
+        //  script tag and _before_ the requirejs script tag. QUnit's non-integration with
+        //  requirejs is depressing to say the least
+        QUnit.load();
+        QUnit.start();
+    });
 
 }());
